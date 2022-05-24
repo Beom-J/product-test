d18970891a998863b9598705b8fea54904268303
Beom-J <beom00032@gmail.com>
2022-05-24 15:39:15 +0900

    feat: axios.ts 세팅

---
+++ src/plugins/axios.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import cookie from 'js-cookie';
+
+axios.defaults.baseURL = '/api';
+
+const tokenFromCookie = cookie.get('access_token');
+//TODO axios 버전 0.27.2 에서는 Authorization 안먹음!! 알아보기
+axios.defaults.headers.Authorization = `Bearer ${tokenFromCookie}`;
+
+export const accessToken = tokenFromCookie;
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function getAccessToken() {
+  console.log('axios:::::', axios.defaults.baseURL);
+  let token = cookie.get('access_token');
+  let tryCnt = 0;
+
+  while (token === undefined) {
+    await delay(100);
+    token = cookie.get('access_token');
+    tryCnt += 1;
+
+    // 2초 이상 획득 실채 시 에러
+    if (tryCnt > 20) {
+      return undefined;
+    }
+  }
+  // TODO 그냥 axios 랑 react-axios 랑 다른가??
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
