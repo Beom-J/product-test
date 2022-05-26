b9fd5c422fa77a1de8c40f54c29f399868267fec
Beom-J <beom00032@gmail.com>
2022-05-26 17:18:01 +0900

    refactor: 미사용 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from 'react-query';
-import { Navigate, useLocation } from 'react-router-dom';
 
 import queryClient from './plugins/queryClient';
 import Toast from './components/ui/toast';
