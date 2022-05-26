4d1c38987e36bdc8e3a1574be40a7c1098273a70
Beom-J <beom00032@gmail.com>
2022-05-26 17:21:17 +0900

    feat: 로그인 여부에 따라 홈 버튼 내용 변경

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@mui/material';
+import { getAccessToken } from '../../plugins/axios';
 
 const Home = () => {
   const navigate = useNavigate();
-  const handleClickLogin = () => {
+  const hasToken = getAccessToken();
+
+  const [introButtonText, setIntroButtonText] = useState('Log in');
+
+  useEffect(() => {
+    if (hasToken) {
+      console.log(typeof hasToken);
+      setIntroButtonText('Welcome!');
+    } else {
+      setIntroButtonText('Log in');
+    }
+  }, []);
+
+  const handleClickButton = () => {
+    if (hasToken) {
+      navigate('/dashboard');
+    }
     navigate('/login');
   };
   return (
     <div>
       <h1>Home 입니다 public 이죠</h1>
 
-      <Button type="button" variant="contained" onClick={handleClickLogin}>
-        log in
+      <Button type="button" variant="contained" onClick={handleClickButton}>
+        {introButtonText}
       </Button>
     </div>
   );
