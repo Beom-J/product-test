99c345cbab61d4ef3d40cd66a9da7279fddd7922
Beom-J <beom00032@gmail.com>
2022-05-26 17:18:40 +0900

    fix: route 호출을 레이아웃으로 감싸는 작업

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DashboardLayout from '../layouts/dashboard';
 import Dashboard from '../pages/dashboard';
 import Home from '../pages/home';
 
 import LoginPage from '../pages/login';
+import Product from '../pages/product';
 import PrivateRoute from './privateRoute';
 
 const Router = () => {
@@ -20,17 +22,15 @@ const Router = () => {
 
         {/* 인증 해야 접근 가능한 페이지 */}
         <Route element={<PrivateRoute authentication={true} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-
-          {/* 권한이 필요한 페이지 */}
-          {/* <Route element={<ProtectedRoute/>}>
-              <Route path='/admin' element={<AdminMainPage />}/>
-              <Route path='/admin/member' element={<ManagerMainPage />}/>
-            </Route> */}
+          <Route
+            path="/dashboard"
+            element={<DashboardLayout children={<Dashboard />} />}
+          />
+          <Route
+            path="/product"
+            element={<DashboardLayout children={<Product />} />}
+          />
         </Route>
-
-        {/* 인증/권한 상관없이 접근 가능한 error 페이지 */}
-        {/* <Route path='/*' element={<ErrorPage/>}/> */}
       </Routes>
     </BrowserRouter>
   );
