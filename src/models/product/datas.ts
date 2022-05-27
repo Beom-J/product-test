7de6c0e26fff8461512ceb40b4ff6423d63a8c23
Beom-J <beom00032@gmail.com>
2022-05-27 17:03:15 +0900

    feat: product data type 변경

--- src/models/product/datas.ts
+++ src/models/product/datas.ts
@@ -3,8 +3,8 @@ export type Product = {
   name: string;
   sku: string;
   upc: string;
-  created_at?: string;
-  updated_at?: string;
+  created_at: string;
+  updated_at: string;
 };
 
 export const KEY_PRODUCT_LIST = 'product_list';
