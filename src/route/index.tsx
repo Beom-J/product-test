6ab2acc6ab8800cae79fa525cc220ee49a742949
Beom-J <beom00032@gmail.com>
2022-05-30 01:22:36 +0900

    refactor: route 레이아웃 구조 변경

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -5,8 +5,9 @@ import Dashboard from '../pages/dashboard';
 import Home from '../pages/home';
 
 import LoginPage from '../pages/login';
-import Product from '../pages/product';
+import ProductHeader from '../pages/product';
 import NewForm from '../pages/product/component/new';
+import ProductTable from '../pages/product/component/table';
 import PrivateRoute from './privateRoute';
 
 const Router = () => {
@@ -23,18 +24,14 @@ const Router = () => {
 
         {/* 인증 해야 접근 가능한 페이지 */}
         <Route element={<PrivateRoute authentication={true} />}>
-          <Route
-            path="/dashboard"
-            element={<DashboardLayout children={<Dashboard />} />}
-          />
-          <Route
-            path="/product"
-            element={<DashboardLayout children={<Product />} />}
-          />
-          <Route
-            path="/product/add"
-            element={<DashboardLayout children={<NewForm />} />}
-          />
+          <Route element={<DashboardLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/product" element={<ProductHeader />}>
+              <Route index element={<ProductTable />} />
+              <Route path="add" element={<NewForm />} />
+            </Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
