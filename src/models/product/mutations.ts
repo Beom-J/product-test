d26badc08dd1bc841d2cdfb3931e6f93c3f583d8
Beom-J <beom00032@gmail.com>
2022-05-30 17:08:50 +0900

    feat: product patch 쿼리 생성

--- src/models/product/mutations.ts
+++ src/models/product/mutations.ts
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import { useMutation } from 'react-query';
-import { Product } from './datas';
+import { Product, ProductTable } from './datas';
 
 export const useCreateProduct = () => {
   return useMutation(async (params: Product) => {
     const url = `/product`;
-    const { data } = await axios.post<Product>(url, params);
+    const { data } = await axios.post<ProductTable>(url, params);
 
     return data;
   });
+};
+
+export const useUpdateProduct = () => {
+  return useMutation(
+    async ({ id, params }: { id: number; params: Product }) => {
+      const url = `/product/${id}`;
+      const { data } = await axios.patch<ProductTable>(url, params);
+
+      return data;
+    }
+  );
 };
