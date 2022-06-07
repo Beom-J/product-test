fb47d4e5eaec8c63a45fc7fbb26613cfbd3129c2
Beom-J <beom00032@gmail.com>
2022-06-07 16:20:52 +0900

    feat: App 에 Sentry.ErrorBoundary 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ import queryClient from './plugins/queryClient';
 import Toast from './components/ui/toast';
 
 import Router from './route';
+import { ErrorBoundary } from '@sentry/react';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toast />
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading....</div>}>
+          <Router />
+          <Toast />
+        </Suspense>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
