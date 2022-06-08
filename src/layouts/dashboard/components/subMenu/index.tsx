c8f50fb5be51d0f8674b72bc112b36f9d9d35966
Beom-J <beom00032@gmail.com>
2022-06-08 19:08:19 +0900

    feat: sidebar sub menu 작업 중

---
+++ src/layouts/dashboard/components/subMenu/index.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { SidebarIcons, SidebarListType } from '../sideBar/SidebarListData';
+import { ExpandLessRounded, ExpandMoreRounded } from '@mui/icons-material';
+
+const SidebarLink = styled(Link)`
+  display: flex;
+  color: #78778f;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+  &:hover {
+    background: #ffffff;
+    color: #036dff;
+    cursor: pointer;
+  }
+`;
+
+const SidebarLabel = styled.span`
+  margin-left: 16px;
+`;
+
+const DropdownLink = styled(Link)`
+  background: #eff5f8;
+  height: 60px;
+  padding-left: 3rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #78778f;
+  font-size: 18px;
+  &:hover {
+    background: #ffffff;
+    color: #036dff;
+    cursor: pointer;
+  }
+`;
+
+const SubMenu = ({ title, path, subNav, icon }: SidebarListType) => {
+  const Icon = SidebarIcons[icon];
+  const [openSubNav, setOpenSubnav] = useState(false);
+
+  const showSubnav = () => setOpenSubnav(!openSubNav);
+
+  return (
+    <>
+      <SidebarLink to={path} onClick={subNav && showSubnav}>
+        <>
+          <Icon />
+          <SidebarLabel>{title}</SidebarLabel>
+        </>
+        <>
+          {subNav && openSubNav ? (
+            <ExpandLessRounded />
+          ) : subNav ? (
+            <ExpandMoreRounded />
+          ) : null}
+        </>
+      </SidebarLink>
+      {openSubNav &&
+        subNav?.map((item, index) => {
+          const Icon = SidebarIcons[item.icon];
+          return (
+            <DropdownLink to={item.path} key={index}>
+              <>
+                <Icon />
+                <SidebarLabel>{item.title}</SidebarLabel>
+              </>
+            </DropdownLink>
+          );
+        })}
+    </>
+  );
+};
+
+export default SubMenu;
