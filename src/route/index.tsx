26a47c391dd04227fc2e459b4169937cce23eff5
Beom-J <beom00032@gmail.com>
2022-06-08 19:09:20 +0900

    test: 임시로 대시보드 레이아웃 갈아낌

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DashboardLayout from '../layouts/dashboard';
+import DashboardLayout from '../layouts/dashboard/Dashboard';
 import Dashboard from '../pages/dashboard';
 import Home from '../pages/home';
 
