69d403ac458952eba3cffd8d400cb99420ed1fb8
Beom-J <beom00032@gmail.com>
2022-06-08 11:10:03 +0900

    style: font family 에 noto sans kr 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
+import { Suspense } from 'react';
 import { QueryClientProvider } from 'react-query';
+import { ErrorBoundary } from '@sentry/react';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import queryClient from './plugins/queryClient';
 import Toast from './components/ui/toast';
 
 import Router from './route';
-import { ErrorBoundary } from '@sentry/react';
-import { Suspense } from 'react';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Noto Sans KR', sans-serif"
+  }
+});
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ErrorBoundary>
-        <Suspense fallback={<div>Loading....</div>}>
-          <Router />
-          <Toast />
-        </Suspense>
-      </ErrorBoundary>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <ErrorBoundary>
+          <Suspense fallback={<div>Loading....</div>}>
+            <Router />
+            <Toast />
+          </Suspense>
+        </ErrorBoundary>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
